882ee4acbe52b3c45de4eda6c11c60b7344aacf2
josephnormandev <jnormantransactions@gmail.com>
2022-04-04 00:10:04 -0500

    adds a tacky bike to the page

--- frontend/src/login/page.js
+++ frontend/src/login/page.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Box from "@mui/material/Box";
 import Grid from '@mui/material/Grid';
 import React, {useEffect, useState} from 'react';
 import { useTheme } from '@mui/material/styles';
@@ -6,6 +7,8 @@ import { useTheme } from '@mui/material/styles';
 import LoginForm from './form';
 import generateURL from "../utils/url-generator";
 import {getBearerToken} from "../utils/jwt-store";
+
+import bikePNG from '../images/bicycle.png';
 
 function LoginPage() {
     const theme = useTheme();
@@ -39,9 +42,22 @@ function LoginPage() {
             alignItems="center"
             backgroundColor={ theme.palette.primary.light }
         >
-            <Grid item xs={ 12 }>
+            <Grid item xs={ 12 } zIndex={ 10 }>
                 <LoginForm />
             </Grid>
+            <Box
+                position="absolute"
+                width={600}
+                height={360}
+                right={25}
+                bottom={25}
+                style={{
+                    zIndex: 1,
+                    backgroundImage: `url(${bikePNG}`,
+                    backgroundSize: 'contain',
+                    backgroundRepeat: 'no-repeat',
+                }}
+            />
         </Grid>
     );
 }
